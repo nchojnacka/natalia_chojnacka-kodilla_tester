e670b19f4394192a9dc41ec603dd8b19f385319f
Natalia Chojnacka <natalia.chojnacka1995@gmail.com>
2020-01-04 20:17:28 +0100

    simpleArray

--- kodilla-intro/src/main/java/SimpleArray.java
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -1,16 +1,20 @@
 public class SimpleArray {
 
-    String book;
-
     public static void main(String[] args) {
 
         String[] books = new String[5];
+        String book = books[3];
+        int numberOfElements = books.length;
 
         books[0] = "Clean Code";
         books[1] = "Little Prince";
-        books[2] = "Kill a Mockingbird";
+        books[2] = "Lord of the rings";
         books[3] = "Crime and punishment";
         books[4] = "Harry Potter";
 
+        System.out.println(book);
+        System.out.println("Moja tablica zawiera " + numberOfElements + " elementów");
+
     }
+
 }
