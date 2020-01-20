c6aa771e5ebb61fc0cf3ffba6a1695f537e04cc8
Natalia Chojnacka <natalia.chojnacka1995@gmail.com>
2020-01-20 17:03:09 +0100

    abstract hw

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Circle.java
@@ -0,0 +1,24 @@
+package com.kodilla.abstracts.homework;
+
+public class Circle extends Shape {
+    private final double radius;
+    final double pi = Math.PI;
+
+    public Circle() {
+        this(1);
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return pi * Math.pow(radius, 2);
+    }
+
+    public double perimeter() {
+        return 2 * pi * radius;
+    }
+
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Rectangle.java
@@ -0,0 +1,24 @@
+package com.kodilla.abstracts.homework;
+
+public class Rectangle extends Shape {
+    private final double width, length;
+
+    public Rectangle() {
+        this(1,1);
+    }
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    @Override
+    public double area() {
+        return width * length;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (width + length);
+    }
+}
