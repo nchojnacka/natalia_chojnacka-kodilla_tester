bba17b6b2962ab6896180174ded80211751e7777
Natalia Chojnacka <natalia.chojnacka1995@gmail.com>
2020-02-11 23:00:34 +0100

    CarRace v2

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/CarRace.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/CarRace.java
@@ -14,11 +14,9 @@ public class CarRace {
     }
 
     public static void doRace(Car car) {
-
         for (int i = 0; i < 3; i++) {
             car.increaseSpeed();
         }
-
         for (int i = 0; i < 2; i++) {
             car.decreaseSpeed();
         }
