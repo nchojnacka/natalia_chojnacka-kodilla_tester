c0099a729bb7b1209fe1c117cb342eae7cab5612
Natalia Chojnacka <natalia.chojnacka1995@gmail.com>
2020-02-24 22:19:53 +0100

    aaa

--- kodilla-collections/kodilla-collections-advanced/src/main/java/com/kodilla/collestions/adv/immutable/ImmutableExample.java
+++ kodilla-collections/kodilla-collections-advanced/src/main/java/com/kodilla/collestions/adv/immutable/ImmutableExample.java
@@ -1,4 +1,8 @@
 package com.kodilla.collestions.adv.immutable;
 
 public class ImmutableExample {
+    public static void main(String[] args) {
+        Book book = new Book("John Stewart", "The last chance");
+
+    }
 }
--- kodilla-collections/kodilla-collections-advanced/src/main/java/com/kodilla/collestions/adv/immutable/homework/Task.java
+++ kodilla-collections/kodilla-collections-advanced/src/main/java/com/kodilla/collestions/adv/immutable/homework/Task.java
@@ -1,4 +1,20 @@
 package com.kodilla.collestions.adv.immutable.homework;
 
 public class Task {
+
+    protected final String title;
+    protected final int duration;
+
+    public final String getTitle() {
+        return title;
+    }
+
+    public final int getDuration() {
+        return duration;
+    }
+
+    public Task(String title, int duration) {
+        this.title = title;
+        this.duration = duration;
+    }
 }
