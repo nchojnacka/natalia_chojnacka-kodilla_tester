bc260bf5712a74aa2be27e4505fa0809a259ee60
Natalia Chojnacka <natalia.chojnacka1995@gmail.com>
2020-03-16 23:33:19 +0100

    parametrized tests hw

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringValidatorTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.parametrized_tests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringValidatorTestSuite {
+
+    private StringValidator validator = new StringValidator();
+
+    @ValueSource(strings = {"", "   "})
+    public void shouldReturnTrueIfStringIsEmpty(String text) {
+        assertTrue(validator.isBlank(text));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void shouldReturnTrueIfStringIsNull(String text) {
+        System.out.println(" | " + text + " | ");
+        assertTrue(validator.isBlank(text));
+    }
+}
---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserValidatorTestSuite {
+
+    private UserValidator userValidator = new UserValidator();
+
+    @ParameterizedTest
+    @ValueSource(strings = { "Anna1", "Katarzyna2", "anna_1", "katarzyna*1", "katarzyna.anna"})
+    public void validateUsername(String username) {
+        boolean result = userValidator.validateUsername(username);
+        assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"anna1@mail.pl", "katarzyna2@mail.pl", "anna1@mail.com", "katarzyna*1@mail.com", "katarzyna.anna@mail.com"})
+    public void validateRmail(String email) {
+        boolean result = userValidator.validateEmail(email);
+        assertTrue(result);
+    }
+}
