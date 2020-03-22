7bc62de893cb4b699dc16a6ad387d6986beb8a4f
Natalia Chojnacka <natalia.chojnacka1995@gmail.com>
2020-03-22 22:31:24 +0100

    fix to BMI HW

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -1,14 +1,14 @@
 package com.kodilla.parametrized_tests.homework;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTestSuite {
 
     @ParameterizedTest
-    @ValueSource(doubles = {60.6, 50.1, 100.00, 90.99})
+    @MethodSource(value = "com.kodilla.parametrized_tests.homework.DataBMI#dataProviderForTestingBmi")
     public void shouldGetBMI(double height, double weight, String expected) {
         Person person = new Person(height, weight);
         assertEquals(expected, person.getBMI());
