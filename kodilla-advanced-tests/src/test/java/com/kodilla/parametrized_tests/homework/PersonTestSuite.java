7b50e85cdf69773607f1d92b1735429548bdff39
Natalia Chojnacka <natalia.chojnacka1995@gmail.com>
2020-04-06 23:27:22 +0200

    fix

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -1,17 +1,15 @@
-//package com.kodilla.parametrized_tests.homework;
-//
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.MethodSource;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//class PersonTestSuite {
-//
-//    @ParameterizedTest
-//    @MethodSource(value = "com.kodilla.parametrized_tests.homework.BmiData#providedDataForTestingBmi")
-//    public void shouldGetBMI(double height, double weight, String expected){
-//        Person person = new Person(height, weight);
-//        assertEquals(expected, person.getBMI());
-//
-//    }
-//}
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PersonTestSuite {
+
+    @ParameterizedTest
+    @MethodSource(value = "com.kodilla.parametrized_tests.homework.BmiData#personList")
+    public void shouldGetBMI(Person  person, String expected){
+        assertEquals(expected, person.getBMI());
+    }
+}
